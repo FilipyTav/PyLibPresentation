85275e5fc9e7d06760fda712081266042253b945
Filipy <filipytav17@gmail.com>
2026-06-16 22:42:14 -0300

    Adds more to the app

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
 import sys
+import httpx
+import json
 from textual.app import App, ComposeResult
+from textual.containers import Container, Horizontal
 from textual.widgets import (
+    Header,
+    Footer,
     Button,
+    Input,
+    TabbedContent,
+    TabPane,
+    TextArea,
 )
 
 
@@ -15,10 +24,40 @@ class APISandbox(App):
     BINDINGS = [("q", "quit", "Sair")]
 
     def compose(self) -> ComposeResult:
-        pass
+        yield Header()
+
+        yield Horizontal(
+            Input(value="https://jsonplaceholder.typicode.com/posts/1", id="url-input"),
+            Button("Executar", id="run-btn", variant="primary"),
+            id="request-bar",
+        )
+
+        with TabbedContent(id="response-tabs"):
+            yield TabPane("Resposta", id="tab-response")
+            yield TabPane("Headers", id="tab-headers")
+
+        yield Footer()
 
     async def on_button_pressed(self, event: Button.Pressed) -> None:
-        pass
+        if event.button.id == "run-btn":
+            url = self.query_one("#url-input", Input).value
+            response_pane = self.query_one("#tab-response", TabPane)
+
+            for child in response_pane.children:
+                await child.remove()
+
+            try:
+                async with httpx.AsyncClient() as client:
+                    response = await client.get(url)
+
+                    pretty_json = json.dumps(response.json(), indent=4)
+
+                    await response_pane.mount(TextArea(pretty_json, read_only=True))
+
+            except Exception as e:
+                await response_pane.mount(
+                    TextArea(f"Erro ao conectar: {str(e)}", read_only=True)
+                )
 
 
 def main() -> int:
